12c769f3bb747d8c61ea62d3454d19fbae7f8f4d
khanhnqse <khanhqn03@gmail.com>
2025-03-19 18:38:53 +0700

    refactor: :art: fix-render

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -140,12 +140,12 @@ export default function OwnerPage() {
           <div className="col-span-2 flex flex-col items-start justify-start gap-2">
             <p className="font-bold">Doanh thu</p>
             <p className="text-[#6F757E] text-xl">
-              {formatCurrency(currentRevenue)}
+              {formatCurrency(currentRevenue ?? 0)}
             </p>
             {percentRevenue >= 0 ? (
               <div className="flex gap-1 items-center justify-start text-green-500 text-sm">
                 <TrendingUp />{" "}
-                <span>{percentRevenue.toFixed(2)}% tháng trước</span>
+                <span>{percentRevenue?.toFixed(2) ?? "Trống"}% tháng trước</span>
               </div>
             ) : (
               <div className="flex gap-1 items-center justify-start text-red-500 text-sm">
@@ -165,7 +165,7 @@ export default function OwnerPage() {
           </div>
           <div className="col-span-2 flex flex-col items-start justify-start gap-2">
             <p className="font-bold">Khách hàng</p>
-            <p className="text-[#6F757E] text-xl">{customerList.length}</p>
+            <p className="text-[#6F757E] text-xl">{customerList?.length ?? "Trống"}</p>
             {percentCustomer >= 0 ? (
               <div className="flex gap-1 items-center justify-start text-green-500 text-sm">
                 <TrendingUp />{" "}
@@ -190,8 +190,7 @@ export default function OwnerPage() {
             </div>
             <div className="col-span-2 flex flex-col items-center justify-center gap-2">
               <p className="font-bold">Số lượng không gian</p>
-              <p className="text-[#6F757E] text-xl">{workspaceList.length}</p>
-            </div>
+              <p className="text-[#6F757E] text-xl">{workspaceList?.length ?? "Trống"}</p>            </div>
           </div>
           <div className="col-span-1 rounded-xl bg-white grid gap-4 md:grid-cols-3 p-4 md:min-h-28">
             <div className="col-span-1 flex items-center justify-center bg-[#fcba03] rounded-xl text-white">
@@ -199,8 +198,7 @@ export default function OwnerPage() {
             </div>
             <div className="col-span-2 flex flex-col items-center justify-center gap-2">
               <p className="font-bold">Số lượng tiện ích</p>
-              <p className="text-[#6F757E] text-xl">{amenityList.length}</p>
-            </div>
+              <p className="text-[#6F757E] text-xl">{amenityList?.length ?? "Trống"}</p>            </div>
           </div>
           <div className="col-span-1 rounded-xl bg-white grid gap-4 md:grid-cols-3 p-4 md:min-h-28">
             <div className="col-span-1 flex items-center justify-center bg-[#FF8E29] rounded-xl text-white">
@@ -208,8 +206,7 @@ export default function OwnerPage() {
             </div>
             <div className="col-span-2 flex flex-col items-center justify-center gap-2">
               <p className="font-bold">Số lượng món</p>
-              <p className="text-[#6F757E] text-xl">{beverageList.length}</p>
-            </div>
+              <p className="text-[#6F757E] text-xl">{beverageList?.length ?? "Trống"}</p>            </div>
           </div>
         </div>
       </div>
