c6837d78e148416e24555cefd827e9b3c59023ba
PhamManhCuong <cuongpham.251203@gmail.com>
2025-04-02 18:18:34 +0700

    feat: :art: update side-bar

--- components/custom-ui/image-upload.tsx
+++ components/custom-ui/image-upload.tsx
@@ -3,7 +3,7 @@
 
 import { useState, useEffect, ChangeEvent } from "react";
 import { Input } from "../ui/input";
-import { X } from "lucide-react";
+import { ImagePlus, X } from "lucide-react";
 
 interface ImageUploadProps {
   value: string;
@@ -17,16 +17,19 @@ const ImageUpload = ({
   handleRemove,
 }: ImageUploadProps) => {
   const [preview, setPreview] = useState<string>("");
+  const [fileNames, setFileNames] = useState("Chưa có hình ảnh nào được chọn");
 
   useEffect(() => {
-    if (typeof value === "string") {
+    if (typeof value === "string" && value !== "") {
       setPreview(value);
+      setFileNames(value);
     }
   }, [value]);
 
   const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file && file.type.startsWith("image/")) {
+      setFileNames(file.name);
       const url = URL.createObjectURL(file);
       setPreview(url);
       handleChange(file);
@@ -35,18 +38,30 @@ const ImageUpload = ({
 
   const removeImage = () => {
     handleRemove();
-    setPreview("");
+    setFileNames("Chưa có ảnh nào được chọn");
   };
 
   return (
     <div className="flex flex-col gap-2">
-      <Input
-        className="py-3 px-4 rounded-md file:bg-seventh border h-[50px]"
-        id="image"
-        type="file"
-        accept="image/*"
-        onChange={handleImageChange}
-      />
+      <div className="flex items-center gap-4 border rounded-md">
+        <label
+          htmlFor="images"
+          className="cursor-pointer py-3 px-5 bg-primary text-white rounded-s-md text-center flex gap-2 items-center"
+        >
+          <ImagePlus /> Chọn hình ảnh
+        </label>
+        <Input
+          className="py-3 px-4 rounded-md file:bg-seventh border h-[50px] hidden"
+          id="images"
+          type="file"
+          accept="image/*"
+          multiple
+          onChange={handleImageChange}
+        />
+        <span className="text-gray-500 text-sm truncate max-w-[400px]">
+          {fileNames}
+        </span>
+      </div>
       {preview && (
         <div className="relative w-36 h-36 border rounded-md overflow-hidden">
           <img
--- lib/zod/schema.ts
+++ lib/zod/schema.ts
@@ -45,12 +45,12 @@ export const workspaceSchema = z
     capacity: z
       .string()
       .refine((val) => !isNaN(Number(val)) && Number(val) >= 1, {
-        message: "Sức chứa tối đa phải >= 1 người",
+        message: "Sức chứa tối đa phải ≥ 1 người",
       }),
     cleanTime: z
       .string()
       .refine((val) => !isNaN(Number(val)) && Number(val) >= 1, {
-        message: "Thời gian dọn dẹp phải >= 1 phút",
+        message: "Thời gian dọn dẹp phải ≥ 1 phút",
       }),
     description: z.string().min(3, "Mô tả không gian phải có ít nhất 3 ký tự"),
     shortTermPrice: z
@@ -82,10 +82,10 @@ export const workspaceSchema = z
       const open = dayjs(data.openTime, "HH:mm");
       const close = dayjs(data.closeTime, "HH:mm");
 
-      return close.diff(open, "hour") >= 1;
+      return close.diff(open, "hour") >= 6;
     },
     {
-      message: "Thời gian đóng cửa phải sau thời gian mở cửa ít nhất 1 giờ",
+      message: "Thời gian đóng cửa phải sau thời gian mở cửa ít nhất 6 giờ",
       path: ["closeTime"],
     }
   );
